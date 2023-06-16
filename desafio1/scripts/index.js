0bbe7218129cdc48beeac2d91e5d33179462742a
Sauloviniciust <sauloviniciust@gmail.com>
2023-06-16 17:08:21 -0300

    criando primeiro desafio

---
+++ desafio1/scripts/index.js
@@ -0,0 +1,15 @@
+let primeiroNumero = prompt(
+    'olá visitante! Digite o primeiro numero a ser somado:'
+    );
+    console.log(primeiroNumero);   
+let segundoNumero = prompt(
+    'olá visitante! Digite o segundo numero a ser somado:'
+    );
+    console.log(segundoNumero);
+
+let soma = parseInt(primeiroNumero) + parseInt(segundoNumero);
+alert(
+    'Meu caro visitante, o resultado da soma é ' + soma
+    );
+
+console.log(soma);
